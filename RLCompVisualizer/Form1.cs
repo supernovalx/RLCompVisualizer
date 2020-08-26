b570e37880fedd021366f1a74c1f94be3b5237f5
supernovalx <hoanganhgo9@gmail.com>
2020-08-26 22:06:26 +0700

    Add project files.

---
+++ RLCompVisualizer/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RLCompVisualizer
+{
+    public partial class Form1 : Form
+    {
+        private PictureBox[,] tiles = new PictureBox[9, 21];
+        private Label[,] labels = new Label[9, 21];
+
+        private Image forest = Image.FromFile("./Assets/forest.png");
+        private Image gold = Image.FromFile("./Assets/gold.png");
+        private Image grass = Image.FromFile("./Assets/grass.png");
+        private Image player = Image.FromFile("./Assets/player.png");
+        private Image swarm = Image.FromFile("./Assets/swarm.png");
+        private Image trap = Image.FromFile("./Assets/trap.png");
+        private const int SIZE = 80;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            using (var fs = new FileStream("D:\\test.txt", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var sr = new StreamReader(fs, Encoding.Default))
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int y = 0; y < 21; y++)
+                    {
+                        if (tiles[i, y] == null)
+                        {
+                            tiles[i, y] = new PictureBox();
+                            tiles[i, y].Size = new Size(SIZE, SIZE);
+                            tiles[i, y].Location = new Point(SIZE * y, SIZE * i);
+                            tiles[i, y].SizeMode = PictureBoxSizeMode.StretchImage;
+                            this.Controls.Add(tiles[i, y]);
+
+                            labels[i, y] = new Label();
+                            //labels[i, y].Size = new Size(SIZE, 10);
+                            labels[i, y].Location = new Point(SIZE * y, SIZE * i);
+                            labels[i, y].Text = "100";
+                            this.Controls.Add(labels[i, y]);
+                        }
+
+                        int type = int.Parse(sr.ReadLine());
+                        if (type == 0)
+                            tiles[i, y].Image = grass;
+                        else if (type == -1)
+                            tiles[i, y].Image = forest;
+                        else if (type == -2)
+                            tiles[i, y].Image = trap;
+                        else if (type == -3)
+                            tiles[i, y].Image = swarm;
+                        else if (type > 0)
+                        {
+                            tiles[i, y].Image = gold;
+                            labels[i, y].Text = "100";
+                            labels[i, y].BringToFront();
+                        }
+
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ RLCompVisualizerWPF/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace RLCompVisualizerWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer = new DispatcherTimer();
+
+        private Image[,] tiles = new Image[9, 21];
+        private Label[,] labels = new Label[9, 21];
+
+        private static string path = @"C:\Users\Admin\source\repos\RLCompVisualizer\RLCompVisualizer\Assets";
+        private BitmapImage forest = new BitmapImage(new Uri($"{path}/forest.png"));
+        private BitmapImage gold = new BitmapImage(new Uri($"{path}/gold.png"));
+        private BitmapImage grass = new BitmapImage(new Uri($"{path}/grass.png"));
+        private BitmapImage player = new BitmapImage(new Uri($"{path}/player.png"));
+        private BitmapImage swarm = new BitmapImage(new Uri($"{path}/swarm.png"));
+        private BitmapImage trap = new BitmapImage(new Uri($"{path}/trap.png"));
+
+        private const int SIZE = 80;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer.Tick += Timer_Tick;
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Start();
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            using (var fs = new FileStream("D:\\test.txt", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var sr = new StreamReader(fs, Encoding.Default))
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int y = 0; y < 21; y++)
+                    {
+                        if (tiles[i, y] == null)
+                        {
+                            tiles[i, y] = new Image();
+                            tiles[i, y].Width = SIZE;
+                            tiles[i, y].Height = SIZE;
+                            Canvas.SetTop(tiles[i, y] , i * SIZE);
+                            Canvas.SetLeft(tiles[i, y] , y * SIZE);
+
+                            
+                            this.grid.Children.Add(tiles[i, y]);
+
+                            labels[i, y] = new Label();
+                            Canvas.SetTop(labels[i, y], i * SIZE);
+                            Canvas.SetLeft(labels[i, y], y * SIZE);
+                            labels[i, y].Content = "";
+                            labels[i, y].Foreground = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                            labels[i, y].FontWeight = FontWeight.FromOpenTypeWeight(700);
+                            labels[i, y].FontSize = 22;
+                            this.grid.Children.Add(labels[i, y]);
+                        }
+
+                        int type = int.Parse(sr.ReadLine());
+                        if (type == 0)
+                            tiles[i, y].Source = grass;
+                        else if (type == -1)
+                            tiles[i, y].Source = forest;
+                        else if (type == -2)
+                            tiles[i, y].Source = trap;
+                        else if (type == -3)
+                            tiles[i, y].Source = swarm;
+                        else if (type > 0)
+                        {
+                            tiles[i, y].Source = gold;
+                            labels[i, y].Content = "100";
+                            //labels[i, y].();
+                        }
+
+                    }
+                }
+
+            }
+        }
+
+        
+
+    }
+}
